ae0a3c0f1b9c2e79e6559b97f597b6538dba98e2
molzania <molzania_1507@yahoo.co.id>
2021-07-29 14:32:24 +0700

    commit on 'main'

--- soal4.js
+++ soal4.js
@@ -1,1 +1,60 @@
-let buah = 
+let buah = [
+    {
+        Pic: './img/apple.png',
+        Name: 'Apple',
+        Color: 'Red',
+        Latin: 'Malus Sylvestris Mill',
+        Seed: 'Single Seed'
+    },
+    {
+        Pic: './img/banana.png',
+        Name: 'Banana',
+        Color: 'Yellow',
+        Latin: 'Musa Paradisiaca',
+        Seed: 'Double Seed'
+    },
+    {
+        Pic: './img/blueberry.png',
+        Name: 'Blueberry',
+        Color: 'Purple',
+        Latin: 'Cyanococcus',
+        Seed: 'Single Seed'
+    },
+    {
+        Pic: './img/cherry.png',
+        Name: 'Cherry',
+        Color: 'Dark Red',
+        Latin: 'Prunus subg. Cerasus',
+        Seed: 'Double Seed'
+    },
+    {
+        Pic: './img/lemon.png',
+        Name: 'Lemon',
+        Color: 'Yellow',
+        Latin: 'Citrus Limun',
+        Seed: 'Single Seed'
+    },
+    {
+        Pic: './img/mango.png',
+        Name: 'Mango',
+        Color: 'Orange',
+        Latin: 'Mangifera indica',
+        Seed: 'Double Seed'
+    },
+    {
+        Pic: './img/orange.png',
+        Name: 'Orange',
+        Color: 'Orange',
+        Latin: 'Citrus',
+        Seed: 'Single Seed'
+    },
+    {
+        Pic: './img/pear.png',
+        Name: 'Pear',
+        Color: 'Green',
+        Latin: 'Pyrus',
+        Seed: 'Double Seed'
+    }
+]
+
+console.log(buah);
