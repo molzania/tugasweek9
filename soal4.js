6137dfaa7246a892ac1a8e92845bc04e8b3ede2a
molzania <molzania_1507@yahoo.co.id>
2021-07-29 15:10:16 +0700

    commit on 'main'

--- soal4.js
+++ soal4.js
@@ -57,7 +57,10 @@ let buah = [
     }
 ]
 
+let list = document.getElementById("list");
 
-
-
-
+data.forEach((item)=>{
+    let li = document.createElement("li");
+    li.innerText = item;
+    list.appendChild(li);
+  })
